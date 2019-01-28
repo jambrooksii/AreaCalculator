715fb10b909fb82857399a4ff449ee5c8a06f2fe
Jamie Brooks <jamiebrooks@Jamies-MBP.hsd1.mi.comcast.net>
2019-01-28 12:39:28 -0500

    Refactoring Code

--- src/com/company/Calculator.java
+++ src/com/company/Calculator.java
@@ -1,11 +1,13 @@
 package com.company;
 
+import org.omg.CORBA.PUBLIC_MEMBER;
+
 public class Calculator {
 
     private double base;
     private double height;
 
-    public double calculateArea(double length, double width, String shaped) {
+    public double calculateArea(String shaped, double length, double width) {
         if (shaped.toLowerCase().equals("rectangle")) {
             double rectangle = length * width;
             return rectangle;
@@ -18,25 +20,40 @@ public class Calculator {
         return -1;
     }
 
-    public double calculateTriangle(double width, double base, String shape1){
-        double triArea = (base / 2) * width;
+    public double calculateTriangle(double height, double base) {
+        double triArea = (base / 2) * height;
         return triArea;
     }
 
-public void setBase(double baseSet){
-        base = baseSet;
-}
-public double getBase(){
+    public void calculateCircle(double radius) {
+        double diameter = radius * 2;
+        double circumfrence = diameter * Math.PI;
+        double circleArea = (radius * radius) * Math.PI;
+
+        System.out.println("Your circle Diameter is " + diameter + " \n\n Your circles circumfrence is " + circumfrence
+                + " \n\n Your circle area is " + circleArea + "\n\n");
+    }
+
+    public double trapazoid(double base1, double base2, double height1){
+        double trapArea = ((base1 + base2)/2)*height1;
+        return trapArea;
+    }
+
+    public double getBase() {
         return base;
-}
+    }
 
-public void setHeight(double heightSet){
-        height = heightSet;
-}
-public double getHeight(){
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public double getHeight() {
         return height;
-}
+    }
 
+    public void setHeight(double height) {
+        this.height = height;
+    }
 }
 
 
--- src/com/company/CalculatorDemo.java
+++ src/com/company/CalculatorDemo.java
@@ -18,6 +18,9 @@ package com.company;
         whether the shape parameter is “triangle” or “rectangle”, and should return the area
         */
 
+
+import java.util.Scanner;
+
 public class CalculatorDemo {
 
     public static void main(String[] args) {
@@ -26,16 +29,64 @@ public class CalculatorDemo {
         Calculator triangleCalc = new Calculator();
         Calculator newCalc1 = new Calculator();
 
-        triangleCalc.setBase(12.3);
-        triangleCalc.setHeight(15.5);
+        Scanner newScan = new Scanner(System.in);
+        Scanner scanNew = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+
+        int tryAgain;
+        int whichShape;
+        do {
+            do {
+                System.out.println("Press [1] to build a Triangle \n Press [2] to Build a Square or Rectangle \n Press [3] for Circle \n Press [4] for Trapezoid");
+                whichShape = newScan.nextInt();
+                if (whichShape < 1 || whichShape > 4) {
+                    System.out.println(" Try again Please enter 1-3");
+                } else
+                    switch (whichShape) {
+                        case 1:
+                            System.out.println("Please Enter the Base dimensions of your Triangle");
+                            double baseNum = scanNew.nextDouble();
+                            System.out.println("Please Enter the Height of your Triangle ");
+                            double triHeight = scanNew.nextDouble();
+                            System.out.println("The Area of your Triangle is " + newCalc.calculateTriangle(triHeight, baseNum) + "\n\n");
+                            break;
+
+                        case 2:
+                            System.out.println("Please enter the Shape you want to build \n Square or Rectangle");
+                            String shapeName = scanner.nextLine();
+                            System.out.println("Please enter the Length of your shape");
+                            Double shapeLength = newScan.nextDouble();
+                            System.out.println(("Please Enter Your shapes Height"));
+                            System.out.println("\n The area of your shape is " + triangleCalc.calculateArea(shapeName, shapeLength, scanNew.nextDouble()) + "\n\n");
+                            break;
+
+                        case 3:
+                            System.out.println("Please Enter the Radius of your Circle");
+                            double cirRadius = scanNew.nextDouble();
+                            newCalc1.calculateCircle(cirRadius);
+                            break;
+                        case 4:
+                            System.out.println("Please Enter the Base1 dimensions of your Trapezoid");
+                            double trapBase1 = scanNew.nextDouble();
+                            System.out.println("Please Enter the Base2 of your Triangle ");
+                            double trapBase2 = scanNew.nextDouble();
+                            System.out.println("Please enter the Height of your Trapezoid");
+                            double trapHeight = scanNew.nextDouble();
+                            System.out.println("The Area of your Trapezoid is " + newCalc1.trapazoid(trapBase1, trapBase2, trapHeight) + "\n\n\n");
+                            break;
+
+                    }
 
 
+            } while (whichShape < 1 || whichShape > 4);
 
+            System.out.println("Do you want to build another Shape \n [1] For yes \n [2] For no");
+            tryAgain = newScan.nextInt();
 
-        System.out.println(newCalc.calculateArea(4, 6, "rectangle"));
-        System.out.println("The Area of your triangle is  " + newCalc1.calculateTriangle(5, 7, "triangle"));
-      //  System.out.println((triangleCalc.getBase() /2) * triangleCalc.getHeight());
-
+        } while (tryAgain == 1);
     }
 
+
 }
+
+
